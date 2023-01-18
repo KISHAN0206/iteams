6faac89fc5909d690232fcd720dfcd9f930e0649
KISHAN AKBARI <kishanakbari977@gmail.com>
2023-01-18 22:43:22 +0530

    addinitem

--- lib/catalog/catalog.dart
+++ lib/catalog/catalog.dart
@@ -1,22 +1,109 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+
+
 
 class catalogmodel {
-  static final item = [
-    Item(
-        1,
-        "S22 Ultra",
-        "Samsung Galaxy S22 ultra 1 TB",
-        "https://images.samsung.com/is/image/samsung/p6pim/levant/2202/gallery/levant-galaxy-s22-ultra-s908-413037-sm-s908ezkgmea-530970280",
-        10000),
+  static List<Item> items = [
+     Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
   ];
 }
 
 class Item {
-  final int id;
+ final int id;
   final String name;
   final String desc;
-  final String image;
   final num price;
+  final String color;
+  final String image;
 
-  Item(this.id, this.name, this.desc, this.image, this.price);
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      desc: map['desc'] as String,
+      price: map['price'] as num,
+      color: map['color'] as String,
+      image: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.name == name &&
+      other.desc == desc &&
+      other.price == price &&
+      other.color == color &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      desc.hashCode ^
+      price.hashCode ^
+      color.hashCode ^
+      image.hashCode;
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,31 +1,54 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
+import 'dart:convert';
+
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
 import 'package:pro/catalog/catalog.dart';
 import 'package:pro/widgets/item_widget.dart';
 import 'widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    catalogmodel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => catalogmodel.item[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
       body: Container(
-        color: Color.fromARGB(255, 153, 134, 187),
-        child: ListView.builder(
-            itemCount: dummyList.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:ItemWidget(
-                    item: dummyList[index],
+          color: Color.fromARGB(255, 153, 134, 187),
+          child: (catalogmodel.items != null && catalogmodel.items.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: catalogmodel.items.length,
+                  itemBuilder: (context, index) => ItemWidget(
+                    item: catalogmodel.items[index],
                   ),
-              );
-            }),
-      ),
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
       drawer: MyDrawer(),
     );
   }
